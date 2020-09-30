b0280a5eb33a125fb5401dd5d8ba6f9db8a66c2a
Felipe Gutierrez <felipeg@ibm.com>
2020-09-30 17:32:58 -0400

    updated

--- setup/concourse-setup.sh
+++ setup/concourse-setup.sh
@@ -6,9 +6,11 @@ echo "** Starting setup as: $(whoami)"
 
 # Ubuntu Box
 echo "** 1/8 Ubuntu"
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt update > /dev/null 2>&1
 apt upgrade -y > /dev/null 2>&1
-apt install wget net-tools postgresql postgresql-contrib -y > /dev/null 2>&1
+apt install vault wget net-tools postgresql postgresql-contrib -y > /dev/null 2>&1
 
 # Concourse
 echo "** 2/8 Concourse"
@@ -19,6 +21,7 @@ rm -rf concourse-$VERSION-linux-amd64.tgz
 # Environment
 echo "** 3/8 Environment"
 echo "PATH=$PATH:/usr/local/concourse/bin" >> /etc/environment
+echo "VAULT_ADDR=https://192.168.50.10:8200" >> /etc/environment
 source /etc/environment
 
 # Keys
@@ -74,9 +77,77 @@ cat >/etc/systemd/system/concourse_web.service <<-EOF
         WantedBy=default.target
 EOF
 
+cat > /etc/systemd/system/vault.service <<-EOF
+        [Unit]
+        Description="HashiCorp Vault - A tool for managing secrets"
+        Documentation=https://www.vaultproject.io/docs/
+        Requires=network-online.target
+        After=network-online.target
+        ConditionFileNotEmpty=/etc/vault.d/vault.hcl
+
+        [Service]
+        User=vault
+        Group=vault
+        ProtectSystem=full
+        ProtectHome=read-only
+        PrivateTmp=yes
+        PrivateDevices=yes
+        SecureBits=keep-caps
+        AmbientCapabilities=CAP_IPC_LOCK
+        NoNewPrivileges=yes
+        ExecStart=/usr/bin/vault server -config=/etc/vault.d/vault.hcl
+        ExecReload=/bin/kill --signal HUP
+        KillMode=process
+        KillSignal=SIGINT
+        Restart=on-failure
+        RestartSec=5
+        TimeoutStopSec=30
+        StartLimitBurst=3
+        LimitNOFILE=65536
+
+        [Install]
+        WantedBy=multi-user.target
+EOF
+
 # Service Enable/Start
-echo "** 8/8 Start Concourse Web"
+echo "** 8/8 Start Services"
+echo "** > Enable Concourse Web"
 systemctl enable concourse_web.service
 systemctl start concourse_web.service
 
+
+echo "** > Creating Self Certs for Vault Service"
+mkdir -p /etc/vault.d/certs
+mkdir -p /usr/local/share/ca-certificates/vault
+
+curl -sS https://raw.githubusercontent.com/antelle/generate-ip-cert/master/generate-ip-cert.sh | bash -s 192.168.50.10
+cp cert.pem /usr/local/share/ca-certificates/vault/vault.crt
+mv *.pem /etc/vault.d/certs/
+sed -i 's/127.0.0.1/192.168.50.10/g' /etc/vault.d/vault.hcl
+chown -R vault.vault /etc/vault.d/
+update-ca-certificates
+
+echo "** > Enable Vault Service"
+systemctl enable vault.service
+systemctl start vault.service
+
+echo "** > Init Vault "
+vault operator init > /etc/vault.d/init.file
+chown vault.vault /etc/vault.d/init.file
+cp -r /etc/vault.d /vagrant/
+for i in `cat /etc/vault.d/init.file | grep "Key" | awk '{print $4}'`; do vault operator unseal $i; done
+cat /etc/vault.d/init.file |grep Token |awk '{print $4}' > /vagrant/vault.d/VAULT_TOKEN
+
+echo "** > Creating Concourse Policy"
+cat > /etc/vault.d/concourse-policy.hcl <<-EOF
+path "concourse/*" {
+  capabilities = ["create", "update", "read", "list"]
+}
+EOF
+
+vault policy write concourse /etc/vault.d/concourse-policy.hcl
+vault secrets enable -path=concourse/ kv
+
 echo "** DONE **"
+
+
